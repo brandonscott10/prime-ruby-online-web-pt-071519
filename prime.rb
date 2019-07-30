8a02b3fd505c2cd19283de9ac633568df35a62a8
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-30 21:45:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(x)
     if x < 2
         return false
     else
-      (2..x).to_a.each do |n|
+      (2...x).to_a.each do |n|
         # for n in (2...x).to_a
             if x % n == 0
                return false
